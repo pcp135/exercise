b8ab07ba723af7610bf1b4dc32b9616223238f5b
pcp135 <phil@parsons.uk.com>
2012-06-23 18:35:39 -0400

    added first test for exercise model

--- getfit/tests.py
+++ getfit/tests.py
@@ -1,16 +1,17 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-
+from django.utils import timezone
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	def test_basic_addition(self):
+		"""
+		Tests that 1 + 1 always equals 2.
+		"""
+		self.assertEqual(1 + 1, 2)
+
+class ExerciseTest(TestCase):
+	def test_creating_a_new_exercise(self):
+		exercise = Exercise()
+		exercise.name = "Test exercise"
+		exercise.reps = 10
+		exercise.save()
+
