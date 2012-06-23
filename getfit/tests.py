0d697bf63311e01df8b025f8bfe36fe6dfc1f6bf
pcp135 <phil@parsons.uk.com>
2012-06-23 18:49:14 -0400

    added test for Measure

--- getfit/tests.py
+++ getfit/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
-from getfit.models import Exercise
+from getfit.models import Exercise, Measure
 
 class SimpleTest(TestCase):
 	def test_basic_addition(self):
@@ -23,3 +23,16 @@ class ExerciseTest(TestCase):
 		
 		self.assertEquals(all_exercises[0].name, "Test exercise")
 		self.assertEquals(all_exercises[0].reps, 10)
+		
+class MeasureTest(TestCase):
+	def test_creating_a_new_measure(self):
+		meas = Measure()
+		meas.name = "Reps"
+		meas.save()
+		
+		all_measures = Measure.objects.all()
+		self.assertEquals(len(all_measures),1)
+		
+		self.assertEquals(meas, all_measures[0])
+		
+		self.assertEquals(all_measures[0].name, "Reps")
