b123be9392d3ea0124998ffbff6f02f3c3f1591e
pcp135 <phil@parsons.uk.com>
2012-06-23 18:57:44 -0400

    modified structure of tests

--- getfit/models.py
+++ getfit/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Exercise(models.Model):
 	name = models.CharField(max_length=100)
+	
+class Measure(models.Model):
+	name = models.CharField(max_length=100)
--- getfit/tests.py
+++ getfit/tests.py
@@ -9,30 +9,28 @@ class SimpleTest(TestCase):
 		"""
 		self.assertEqual(1 + 1, 2)
 
-class ExerciseTest(TestCase):
-	def test_creating_a_new_exercise(self):
-		exercise = Exercise()
-		exercise.name = "Test exercise"
-		exercise.reps = 10
-		exercise.save()
+class ModelTests(TestCase):
+	def setUp(self):
+		self.exercise = Exercise()
+		self.exercise.name = "Test exercise"
+		self.exercise.save()
 		
+		self.meas = Measure()
+		self.meas.name = "Reps"
+		self.meas.save()
+		
+	def test_creating_a_new_exercise(self):
 		all_exercises = Exercise.objects.all()
 		self.assertEquals(len(all_exercises),1)
 		
-		self.assertEquals(exercise, all_exercises[0])
+		self.assertEquals(self.exercise, all_exercises[0])
 		
 		self.assertEquals(all_exercises[0].name, "Test exercise")
-		self.assertEquals(all_exercises[0].reps, 10)
 		
-class MeasureTest(TestCase):
 	def test_creating_a_new_measure(self):
-		meas = Measure()
-		meas.name = "Reps"
-		meas.save()
-		
 		all_measures = Measure.objects.all()
 		self.assertEquals(len(all_measures),1)
 		
-		self.assertEquals(meas, all_measures[0])
+		self.assertEquals(self.meas, all_measures[0])
 		
 		self.assertEquals(all_measures[0].name, "Reps")
