a91ad53f5f3ec9dbc5d25f5bcb9d8af6353459d4
pcp135 <phil@parsons.uk.com>
2012-06-23 18:44:05 -0400

    improved first test

--- getfit/tests.py
+++ getfit/tests.py
@@ -15,4 +15,11 @@ class ExerciseTest(TestCase):
 		exercise.name = "Test exercise"
 		exercise.reps = 10
 		exercise.save()
-
+		
+		all_exercises = Exercise.objects.all()
+		self.assertEquals(len(all_exercises),1)
+		
+		self.assertEquals(exercise, all_exercises[0])
+		
+		self.assertEquals(all_exercises[0].name, "Test exercise")
+		self.assertEquals(all_exercises[0].reps, 10)
