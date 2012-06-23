1fe7bffd8eacd0a9b29bac0482ec0fa0e7ee6aaf
pcp135 <phil@parsons.uk.com>
2012-06-23 19:09:10 -0400

    wrote a test to check an exercise can find its measures

--- getfit/tests.py
+++ getfit/tests.py
@@ -19,6 +19,11 @@ class ModelTests(TestCase):
 		self.meas.name = "Reps"
 		self.meas.exercise = self.exer
 		self.meas.save()
+
+		self.meas2 = Measure()
+		self.meas2.name = "Height"
+		self.meas2.exercise = self.exer
+		self.meas2.save()
 		
 	def test_creating_a_new_exercise(self):
 		all_exercises = Exercise.objects.all()
@@ -36,4 +41,10 @@ class ModelTests(TestCase):
 		
 		self.assertEquals(all_measures[0].name, "Reps")
 		self.assertEquals(all_measures[0].exercise, self.exer)
-		self.assertEquals(all_measures[0].exercise.name, "Test exercise")		
+		self.assertEquals(all_measures[0].exercise.name, "Test exercise")
+		
+	def test_exercise_knows_its_measures(self):
+		measures = self.exer.measure_set.all()
+		
+		self.assertIn("Reps", measures)
+		self.assertIn("Height", measures)
