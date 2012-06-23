886de8fc21188fc4ed9cb6f384ce5b187bc43016
pcp135 <phil@parsons.uk.com>
2012-06-23 18:59:49 -0400

    updated test to check for linkage between measures and exercises

--- getfit/tests.py
+++ getfit/tests.py
@@ -11,19 +11,20 @@ class SimpleTest(TestCase):
 
 class ModelTests(TestCase):
 	def setUp(self):
-		self.exercise = Exercise()
-		self.exercise.name = "Test exercise"
-		self.exercise.save()
+		self.exer = Exercise()
+		self.exer.name = "Test exercise"
+		self.exer.save()
 		
 		self.meas = Measure()
 		self.meas.name = "Reps"
+		self.meas.exercise = exer
 		self.meas.save()
 		
 	def test_creating_a_new_exercise(self):
 		all_exercises = Exercise.objects.all()
 		self.assertEquals(len(all_exercises),1)
 		
-		self.assertEquals(self.exercise, all_exercises[0])
+		self.assertEquals(self.exer, all_exercises[0])
 		
 		self.assertEquals(all_exercises[0].name, "Test exercise")
 		
@@ -34,3 +35,4 @@ class ModelTests(TestCase):
 		self.assertEquals(self.meas, all_measures[0])
 		
 		self.assertEquals(all_measures[0].name, "Reps")
+		self.assertEquals(all_measures[0].exercise, self.exer)
