190d0ad57f869b8f20e9f3727c8d08e19b8f5d7d
pcp135 <phil@parsons.uk.com>
2012-07-01 14:04:17 -0400

    setup to use differing databases depending on environement

--- exercise/settings.py
+++ exercise/settings.py
@@ -1,5 +1,6 @@
 # Django settings for exercise project.
 import dj_database_url
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -10,18 +11,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'database.sqlite',                # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
+if os.environ.get('DEVELOPMENT', True):
+	DATABASES = {
+	    'default': {
+	        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+	        'NAME': 'database.sqlite',                # Or path to database file if using sqlite3.
+	        'USER': '',                      # Not used with sqlite3.
+	        'PASSWORD': '',                  # Not used with sqlite3.
+	        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+	        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+	    }
+	}
+else:
+	DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
