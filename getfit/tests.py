529b33d05c7dc679f0446b8d3a486a83eb5ab309
pcp135 <phil@parsons.uk.com>
2012-07-08 12:56:15 -0400

    modified tests to use reverse links

--- getfit/tests.py
+++ getfit/tests.py
@@ -134,37 +134,37 @@ class ViewTests(TestCase):
 		self.score2.save()
 
 	def test_home_page_exists_and_uses_correct_template(self):
-		response = self.client.get('/')
+		response = self.client.get(reverse('getfit.views.home'))
 		template_names_used = [t.name for t in response.templates]
 		self.assertIn('home.html', template_names_used)
 		
 	def test_home_page_displays_exercises_and_dates(self):
-		response = self.client.get('/')
+		response = self.client.get(reverse('getfit.views.home'))
 		self.assertIn(self.work1.exercise.name, response.content)
 		self.assertIn(self.work2.exercise.name, response.content)
 		self.assertIn(self.work1.time_of_workout.astimezone(self.eastern).strftime("%A %d %B %Y"), response.content)
 		self.assertIn(self.work2.time_of_workout.astimezone(self.eastern).strftime("%A %d %B %Y"), response.content)
 		
 	def test_workout_page_exists_and_uses_correct_template(self):
-		response = self.client.get('/workout/%d/' % self.work2.id)
+		response = self.client.get(reverse('getfit.views.workout', args=[self.work2.id,]))
 		template_names_used = [t.name for t in response.templates]
 		self.assertIn('workout.html', template_names_used)
 		
 	def test_home_page_links_to_workout_pages(self):
-		response = self.client.get('/')
+		response = self.client.get(reverse('getfit.views.home'))
 		work1_url = reverse('getfit.views.workout', args=[self.work1.id,])
 		self.assertIn(work1_url, response.content)
 		work2_url = reverse('getfit.views.workout', args=[self.work2.id,])
 		self.assertIn(work2_url, response.content)
 		
 	def test_workout_page_lists_exercise_and_date_and_time_of_workout(self):
-		response = self.client.get('/workout/%d/' % self.work2.id)
+		response = self.client.get(reverse('getfit.views.workout', args=[self.work2.id,]))
 		self.assertIn(self.work2.exercise.name, response.content)
 		self.assertIn(self.work2.time_of_workout.astimezone(self.eastern).strftime("%A %d %B %Y"), response.content)
 		self.assertIn(self.work2.time_of_workout.astimezone(self.eastern).strftime("%H:%M"), response.content)
 		
 	def test_workout_lists_each_score(self):
-		response = self.client.get('/workout/%d/' % self.work1.id)
+		response = self.client.get(reverse('getfit.views.workout', args=[self.work1.id,]))
 		self.assertIn(self.meas1.name, response.content)
 		self.assertIn(self.score2.measure.name, response.content)
 		self.assertIn(str(self.score1.result), response.content)
@@ -174,26 +174,24 @@ class ViewTests(TestCase):
 		form = WorkoutScoreForm(self.work1)
 		self.assertEqual(form.fields.keys(), [self.score1.measure.name, self.score2.measure.name])
 		self.assertEqual([form.fields[c].initial for c in form.fields], [2201, 3220])
-		response = self.client.get('/workout/%d/' % (self.work1.id, ))		
+		response = self.client.get(reverse('getfit.views.workout', args=[self.work1.id,]))		
 		self.assertTrue(isinstance(response.context['form'], WorkoutScoreForm))
 		
 	def test_workout_can_handle_a_change_via_POST(self):
 		post_data = {'Reps': str(22022), 'Time': str(4331)}
-		workout_url = '/workout/%d/' % self.work1.id
-		response=self.client.post(workout_url, data=post_data)
+		response=self.client.post(reverse('getfit.views.workout', args=[self.work1.id,]), data=post_data)
 
 		changedWorkout = Workout.objects.get(pk=self.work1.id)
 		self.assertEquals(changedWorkout.score_set.all()[0].result, 22022)
 		self.assertEquals(changedWorkout.score_set.all()[1].result, 4331)
-		self.assertRedirects(response, '/')
+		self.assertRedirects(response, reverse('getfit.views.home'))
 		
 	def test_homepage_has_a_link_to_add_new_workout(self):
-		response = self.client.get('/')
-		add_url = reverse('getfit.views.add')
-		self.assertIn(add_url, response.content)
+		response = self.client.get(reverse('getfit.views.home'))
+		self.assertIn(reverse('getfit.views.add'), response.content)
 		
 	def test_add_page_exists_and_uses_correct_template(self):
-		response = self.client.get('/workout/add/')
+		response = self.client.get(reverse('getfit.views.add'))
 		template_names_used = [t.name for t in response.templates]
 		self.assertIn('add.html', template_names_used)
 
@@ -201,74 +199,78 @@ class ViewTests(TestCase):
 		form = NewWorkoutForm()
 		self.assertEqual(form.fields.keys(), ["exercise","time_of_workout"])
 		self.assertEqual(form.fields["exercise"].initial, None)
-		response = self.client.get('/workout/add/')		
+		response = self.client.get(reverse('getfit.views.add'))		
 		self.assertTrue(isinstance(response.context['form'], NewWorkoutForm))
 		
 	def test_add_view_can_create_a_new_workout(self):
 		post_data = {'exercise': str(self.exer1.id), 'time_of_workout': "2010-05-01 15:15"}
-		response = self.client.post('/workout/add/', data=post_data)
+		response = self.client.post(reverse('getfit.views.add'), data=post_data)
 		self.assertEquals(len(Workout.objects.all()), 3)
-		self.assertRedirects(response, '/workout/3/')
+		self.assertRedirects(response, reverse('getfit.views.workout', args=[3,]))
 
 		post_data = {'exercise': str(self.exer2.id), 'time_of_workout': "2010-05-01 16:16"}
-		response = self.client.post('/workout/add/', data=post_data)
+		response = self.client.post(reverse('getfit.views.add'), data=post_data)
 		self.assertEquals(len(Workout.objects.all()), 4)
 
-		response = self.client.get('/workout/3/')		
+		response = self.client.get(reverse('getfit.views.workout', args=[3,]))		
 		self.assertIn("Reps:", response.content)
 		self.assertIn("Time:", response.content)
 		
-		response = self.client.get('/workout/4/')		
+		response = self.client.get(reverse('getfit.views.workout', args=[4,]))		
 		self.assertNotIn("Reps:", response.content)
 		self.assertIn("Time:", response.content)
 		
 	def test_add_view_cant_create_workout_with_invalid_date(self):
 		post_data = {'exercise': str(self.exer1.id), 'time_of_workout': "2010-565-01 15:15"}
-		response = self.client.post('/workout/add/', data=post_data)
+		response = self.client.post(reverse('getfit.views.add'), data=post_data)
 		self.assertEquals(len(Workout.objects.all()), 2)
 		self.assertIn('Enter a valid date/time', response.content)
 
 	def test_add_view_cant_create_workout_with_invalid_exercise(self):
 		post_data = {'exercise': "700", 'time_of_workout': "2010-05-01 15:15"}
-		response = self.client.post('/workout/add/', data=post_data)
+		response = self.client.post(reverse('getfit.views.add'), data=post_data)
 		self.assertEquals(len(Workout.objects.all()), 2)
 		self.assertIn('Select a valid choice', response.content)
 		
 	def test_workout_view_has_delete_button(self):
-		response = self.client.get('/workout/%d/' % self.work1.id)
+		response = self.client.get(reverse('getfit.views.workout', args=[self.work1.id,]))
 		self.assertIn("Delete this workout", response.content)
 
 	def test_deleting_a_workout_removes_it(self):
-		response = self.client.get('/workout/%d/delete/' % self.work1.id)
+		response = self.client.get(reverse('getfit.views.delete', args=[self.work1.id,]))
 		self.assertEquals(len(Workout.objects.all()), 1)
-		self.assertRedirects(response, '/')
+		self.assertRedirects(response, reverse('getfit.views.home'))
 		
 	def test_trying_to_open_an_invalid_workout_tells_you_it_doesnt_exist(self):
 		response = self.client.get('/workout/200/')
 		self.assertIn("That workout doesn't exist", response.content)
 
 	def test_trying_to_delete_an_invalid_workout_tells_you_it_doesnt_exist(self):
-		response = self.client.get('/workout/200/delete/')
+		response = self.client.get(reverse('getfit.views.workout', args=[200,]))
 		self.assertIn("That workout doesn't exist", response.content)
 		
 	def test_workout_view_has_edit_button(self):
-		response = self.client.get('/workout/%d/' % self.work1.id)
+		response = self.client.get(reverse('getfit.views.workout', args=[self.work1.id,]))
 		self.assertIn("Edit this workout", response.content)
 
 	def test_editing_a_workout_date_updates_it(self):
 		post_data = {'exercise': str(self.exer1.id), 'time_of_workout': "2010-06-01 15:15"}
-		response = self.client.post('/workout/%d/edit/' % self.work1.id, data=post_data)
+		response = self.client.post(reverse('getfit.views.edit', args=[self.work1.id,]), data=post_data)
 		self.assertEquals(len(Workout.objects.all()), 2)
-		self.assertRedirects(response, '/workout/%d/' % self.work1.id)
+		self.assertRedirects(response, reverse('getfit.views.workout', args=[self.work1.id,]))
 		chgdWorkout = Workout.objects.get(pk = self.work1.id)
 		self.assertEquals("Tuesday 01 June 2010", chgdWorkout.time_of_workout.astimezone(self.eastern).strftime("%A %d %B %Y"))
 
 	def test_editing_a_workout_exercise_updates_it(self):
 		self.assertEquals(self.work1.exercise, self.exer1)
 		post_data = {'exercise': str(self.exer2.id), 'time_of_workout': str(self.work1.time_of_workout.astimezone(self.eastern).strftime('%Y-%m-%d %H:%M'))}
-		response = self.client.post('/workout/%d/edit/' % self.work1.id, data=post_data)
+		response = self.client.post(reverse('getfit.views.edit', args=[self.work1.id,]), data=post_data)
 		self.assertEquals(len(Workout.objects.all()), 2)
-		self.assertRedirects(response, '/workout/%d/' % self.work1.id)
+		self.assertRedirects(response, reverse('getfit.views.workout', args=[self.work1.id,]))
 		chgdWorkout = Workout.objects.get(pk = self.work1.id)
 		self.assertEquals(chgdWorkout.exercise, self.exer2)
-		
+	
+	def test_home_page_has_a_link_back_to_itself(self):
+		response = self.client.get(reverse('getfit.views.home'))
+		self.assertIn('href "' + reverse('getfit.views.home'), response.content)
+
